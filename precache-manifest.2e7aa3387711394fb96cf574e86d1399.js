5a56229e08f4c2d0b9d7703dc3e32bcd1b10d0f5
sunhpark42 <sunhpark42@gmail.com>
2020-08-03 01:56:42 +0900

    Updates

---
+++ precache-manifest.2e7aa3387711394fb96cf574e86d1399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aac59e4fb8d49c160f96c4c77bdaa0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7f8f3915743d1b192bc0",
+    "url": "/movie_app_2020/static/css/main.4c86b5af.chunk.css"
+  },
+  {
+    "revision": "16b5d0dd6f09ec28156d",
+    "url": "/movie_app_2020/static/js/2.b7d50133.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/movie_app_2020/static/js/2.b7d50133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8f3915743d1b192bc0",
+    "url": "/movie_app_2020/static/js/main.9f4f70c6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
